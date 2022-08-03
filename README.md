8c34daefb4e0c089bb125f32d5994aa67dda5db2
CookieProgrammer9 <article.reader988@gmail.com>
2022-08-03 16:19:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-fstlxb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-fstlxb)
---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css'],
+})
+export class TodoComponent implements OnInit {
+  title: string;
+  todoItem: string = '';
+  todoItems: Array<string> = [];
+  searchInput: string = '';
+  searchOutput: Array<string> = [];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.title = 'Todo';
+  }
+
+  addTodo()
+  {
+    if (this.todoItem) 
+    {
+      //console.log("TODO is : " + todo);
+      this.todoItems.push(this.todoItem);
+      this.todoItem = '';
+    }
+  }
+  removeTodo(todo:String)
+  {
+      console.log("Removed: " + todo);
+      this.todoItems = this.todoItems.filter(item => item !== todo)
+  }
+
+  findTodo(todo: string) : string
+  { 
+    return this.todoItems.find(item => item === todo);
+  }
+
+  logMe(todo:String)
+  {
+    console.log("TODO is : " + todo)
+  }
+
+  searchItem() : void
+  {
+    //let temp : Array<string> = ['', ''];
+
+    this.searchOutput = this.todoItems.filter(item => item.toLocaleLowerCase().includes(this.searchInput));;
+
+    console.log('Temp: ' + this.searchOutput);
+
+    //return temp
+  }
+  
+}
